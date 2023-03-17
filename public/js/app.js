e5d0385c0f11854718580619944817af3780ab73
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-03-17 09:59:13 -0500

    add app file

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,43 @@
 // Cargar el modulo HTTP
 var http = require('http');
+const fs= require('fs')
 
 // Configurar una respuesta HTTP para todas las peticiones
+const host='localhost';
+
+const port=3000;
+const server1 = http.createServer((req,res)=>{
+
+  fs.readFile('public/index.html', (err, data)=>{
+
+    if(err){
+
+      res.writeHead(404)
+      res.write("no se encosntro")
+    }else{
+
+      res.write(data)
+    }
+    res.end();
+  })
+  
+
+
+})
 function onRequest(request, response) {
   console.log("Peticion Recibida.");
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("./index.html");
-  response.end();
+  
+ 
 }
 
-var server = http.createServer(onRequest);
+
 
 // Escuchar al puerto 8080
-server.listen(8080);
+server1.listen(port,host, ()=>{
+  console.log("Servidor funcionando en ",host, port);
+
+});
 
 // Poner un mensaje en la consola
-console.log("Servidor funcionando en http://localhost:8080/");
+//console.log("Servidor funcionando en http://localhost:8080/");
