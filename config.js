a118f2e96a74b0ba9ab130e203937ed505d5f529
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-03-19 08:16:57 -0500

    add config file

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    HOST: process.env.HOST || 'paginaprueba-658fo.paginaprueba-658fo.svc.cluster.local',
+    HOST: process.env.HOST || '127.0.0.1',
     PORT: process.env.PORT ||8080
   }
